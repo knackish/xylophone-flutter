5fe44ae337bfef60a31743799699b13faea95794
Kelvin <kelvinw@iastate.edu>
2019-09-04 16:47:19 -0500

    created playSound function, added background color

--- lib/main.dart
+++ lib/main.dart
@@ -5,60 +5,60 @@ import 'package:audioplayers/audio_cache.dart';
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+
+  void playSound(int soundNum) {
+    final player = AudioCache();
+    player.play('note$soundNum.wav');
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.black,
         body: SafeArea(
           child: Column(
             children: <Widget>[ 
               FlatButton(
                 color: Colors.red,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note1.wav');
+                  playSound(1);
                 }, 
               ),
               FlatButton(
                 color: Colors.orange,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note2.wav');
+                playSound(2);
                 }, 
               ),
               FlatButton(
                 color: Colors.yellow,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note3.wav');
+                playSound(3);
                 }, 
               ),
               FlatButton(
                 color: Colors.green,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note4.wav');
+                playSound(4);
                 }, 
               ),
               FlatButton(
                 color: Colors.teal,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note5.wav');
+                playSound(5);
                 }, 
               ),
               FlatButton(
                 color: Colors.blue,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note6.wav');
+                playSound(6);
                 }, 
               ),
               FlatButton(
                 color: Colors.purple,
                 onPressed: () {
-                final player = AudioCache();
-                player.play('note7.wav');
+                playSound(7);
                 }, 
               ),
             ],
